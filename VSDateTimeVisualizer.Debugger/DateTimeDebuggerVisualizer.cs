c2e43fb91f70d08e81a1ce082213bbf3a96cd421
James Duda <jimbojim1997@gmail.com>
2024-01-20 15:16:57 +0000

    Add project files.

---
+++ VSDateTimeVisualizer.Debugger/DateTimeDebuggerVisualizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.VisualStudio.DebuggerVisualizers;
+
+namespace VSDateTimeVisualizer.Debugger
+{
+    public class DateTimeDebuggerVisualizer : DialogDebuggerVisualizer
+    {
+        public DateTimeDebuggerVisualizer(FormatterPolicy preferredFormatterPolicy) : base(preferredFormatterPolicy)
+        {
+        }
+
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
